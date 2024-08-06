76802d4ee7d3645c7c8e5e237dfe6fda33c30495
Nnete Phasha <nnetephasha@gmail.com>
2024-08-06 14:43:28 +0200

    Router added

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -11,19 +11,19 @@ const HeroSection = () => {
         <div className="carousel-item active">
           <img src={image1} className="d-block w-100" alt="Slide 1" />
           <div className="carousel-caption d-none d-md-block">
-            <h1 className="display-4 text-center">Welcome to Our Website</h1>
+            <h1 className="display-4 text-center fw-bold">Welcome to Our Website</h1>
           </div>
         </div>
         <div className="carousel-item">
           <img src={image2} className="d-block w-100" alt="Slide 2" />
           <div className="carousel-caption d-none d-md-block">
-            <h1 className="display-4 text-center">Explore Our Services</h1>
+            <h1 className="display-4 text-center fw-bold">Premium Trailers</h1>
           </div>
         </div>
         <div className="carousel-item">
           <img src={image3} className="d-block w-100" alt="Slide 3" />
           <div className="carousel-caption d-none d-md-block">
-            <h1 className="display-4 text-center">Contact Us Today</h1>
+            <h1 className="display-4 text-center fw-bold">Contact Us Today</h1>
           </div>
         </div>
       </div>
